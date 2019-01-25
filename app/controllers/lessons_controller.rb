4462885425d1abcfaa59417ec6f378fdafac47b0
Jake Rickard <jakelrickard@gmail.com>
2019-01-25 03:31:17 +0000

    lessons verification

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,14 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
+  end
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course != current_user
+      redirect_to root_url, alert: 'Sign up for the Course!' 
+    end
   end
 
   private
